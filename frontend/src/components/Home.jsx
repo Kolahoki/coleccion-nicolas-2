898533d06620312307197650028e8839e827f705
Kolahoki <nicolax449@gmail.com>
2023-11-07 17:54:24 +0000

    Varias páginas y persistencia datos terminado.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,26 +1,37 @@
 import React from "react";
-import { Typography } from "@mui/material";
 import { useSelector } from "react-redux";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 function Home() {
-    const userData = useSelector(state => state.login)
-    const isLoggedin = userData.isAutenticated
-    const navigate = useNavigate()
+  const userData = useSelector((state) => state.login);
+  const isLoggedin = userData.isAutenticated;
+  const navigate = useNavigate();
 
-    useEffect(() => {
+  useEffect(() => {
+    if (!isLoggedin) {
+      navigate("/");
+    }
+  }, [isLoggedin, navigate]);
 
-        if (!isLoggedin) {
-            navigate('/')
-        }
-
-    }, [isLoggedin, navigate])
-
-    console.log(userData)
-    return <>
-        <Typography variant="h1">Hola {userData.userName} como rol: {userData.userRol} </Typography>
+  console.log(userData);
+  return (
+    <>
+      <h1>Página home de Nicolás Borysov Ravelo</h1>
+      <h2>
+        Hola {userData.userName} como rol: {userData.userRol}{" "}
+      </h2>
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Salir
+      </Button>
     </>
+  );
 }
 
 export default Home;
